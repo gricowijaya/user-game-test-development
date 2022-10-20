a5373eef8de807c377a320c181c8313656e8b590
Gede Rico Wijaya <gricowijaya@gmail.com>
2022-10-20 10:45:05 +0800

    add example

---
+++ index.js
@@ -0,0 +1,32 @@
+require('dotenv').config();
+const express = require('express');
+const morgan = require('morgan');
+const router = require('./routes');
+const app = express();
+
+const {
+    HTTP_PORT
+} = process.env;
+
+app.use(express.json());
+app.use(morgan('dev'));
+app.use(router)
+
+// 404 handler
+app.use((req, res, next) => {
+    return res.status(404).json({
+        status: false,
+        message: 'Are you lost?'
+    });
+});
+
+// 500 handler
+app.use((err, req, res, next) => {
+    console.log(err);
+    return res.status(500).json({
+        status: false,
+        message: err.message
+    });
+});
+
+app.listen(HTTP_PORT, () => console.log('listening on port', HTTP_PORT));
